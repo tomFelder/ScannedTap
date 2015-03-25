1a7b6c9a7e1aced3412718d5725920db928800b1
tomFelder <tom.frauenfelder@gmail.com>
2015-03-25 23:07:07 +1100

    sketchfab API added

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-var iframe = document.getElementById( 'api-frame' );
+var iframe = $( '#api-frame' )[ 0 ];
 var version = '1.0.0';
+var urlid = '6cd4dd83c8f84e098271e8b43330753c';
+
 var client = new Sketchfab( version, iframe );
 
+var error = function () {
+    console.error( 'Error api Sketchfab !' );
+};
 
-//var urlid = '7w7pAfrCfjovwykkEeRFLGw5SXS';
+console.log('pre api start')
 
-var urlid = 'bef9ecc8977c49f6a704c401a27433da'
+var success = function ( api ) {
 
-client.init( urlid, {
+    api.start( function () {
+        $( '#start' ).click( function () {
+            api.start();
+        } );
+
+        $( '#stop' ).click( function () {
+            api.stop();
+        } );
+    } );
+
+};
+
+console.log('api started - initialising client')
+
+client.init( '6cd4dd83c8f84e098271e8b43330753c', {
     success: function onSuccess( api ){
         //API is ready to use
         api.start();
@@ -16,20 +35,6 @@ client.init( urlid, {
         console.log( 'Viewer error' );
     }
 } );
+    
+console.log('client intialised')    
 
-
-client.init( urlid, {
-    success: function onSuccess( api ){ },
-    error: function onError() { },
-    camera: 0,
-    blending: 1,
-    autospin: 4,
-    annotation_cycle: 4,
-    transparent: 0
-} );
-
-api.lookat(
-    [ 0, 13, 10], // eye position
-    [0, 10, 0],   // target to lookat
-    4.3           // duration of the animation in seconds
-);
